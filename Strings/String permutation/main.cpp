91cb532ea6e22a0632192ac95fe6d4cc9f043897
Harsh Balpande <harshbalpande55@gmail.com>
2020-06-21 13:45:31 +0530

    permutation of string

---
+++ Strings/String permutation/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void per(char s[],int k)
+  {
+    char a[10]={0};
+    char res[10];
+    int i;
+    if (s[k]=='\0')
+    {
+        res[k]='\0';
+        cout<<res;
+    }
+    else
+    {
+      for(i=0;s[i]='\0';i++)
+        {
+            if (a[i]==0)
+            {
+                res[k]=s[i];
+                a[i]=1;
+                per(s,k+1);
+                a[i]=0;
+            }
+        }
+
+    }
+
+}
+
+int main()
+{
+    char s[]="ABC";
+
+    per(s,0);
+    return 0;
+}
